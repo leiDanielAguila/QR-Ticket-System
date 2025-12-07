6886d6fcc18274d2aa77eafcb27a6c55ea96ef3c
christlei aguila <aguila.christleidaniel@gmail.com>
2025-12-07 18:28:51 +0800

    feat: added users table with relationship to events table

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-from models import Event, Event_DB
+from models import Event, Event_DB, Guest, Guest_DB
 from database import engine, Base
 from dependencies import get_db
 from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine)
-
-name = 'lei'
-
 
 app = FastAPI()
 
@@ -36,3 +33,22 @@ async def create_event(event: Event, db: Session = Depends(get_db)):
     db.commit()
     db.refresh(new_event)
     return new_event
+
+
+@app.get("/api/events/")
+async def get_events(db: Session = Depends(get_db)):
+    return db.query(Event_DB).all()
+
+
+@app.post("/api/guest")
+async def create_guest(guest: Guest, db: Session = Depends(get_db)):
+    new_guest = Guest_DB(
+        guest_name = guest.guest_name,
+        email = guest.email,
+        event_id = guest.event_id
+    )
+    db.add(new_guest)
+    db.commit()
+    db.refresh(new_guest)
+    return new_guest
+    
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 from pydantic import BaseModel
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey
 from database import Base
+from sqlalchemy.orm import relationship
 
 class Event(BaseModel):
     event_name: str
@@ -17,3 +18,22 @@ class Event_DB(Base):
     event_date = Column(String, index=True)
     venue = Column(String, index=True)
     guest_count = Column(Integer, index=True)
+
+    guests = relationship("Guest_DB", back_populates="events")
+
+
+class Guest(BaseModel):
+    id: int
+    guest_name: str
+    email: str
+    event_id: int
+
+
+class Guest_DB(Base):
+    __tablename__ = "guest"
+    id = Column(Integer, primary_key=True)
+    guest_name = Column(String, index=True)
+    email = Column(String, index=True)
+    event_id = Column(Integer, ForeignKey("events.id"),index=True)
+
+    events = relationship("Event_DB", back_populates="guests")
